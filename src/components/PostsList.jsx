9590b073f2aa2d52fe28480f8c71d663030cf410
nyanky0 <hwngkyo@gmail.com>
2024-04-17 11:05:26 +0700

    post data

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -4,16 +4,8 @@ import classes from './PostsList.module.css';
 import NewPost from "./NewPost";
 import Modal from './Modal';
 
-function PostsList({isPosting, onStopPosting}) {
-    const [enteredBody, setEnteredBody] = useState('');
-    const [enteredAuthor, setEnteredAuthor] = useState('');
+function PostsList({ isPosting, onStopPosting }) {
 
-    function bodyChangeHandler(event) {
-        setEnteredBody(event.target.value);
-    }
-    function authorChangeHandler(event) {
-        setEnteredAuthor(event.target.value);
-    }
 
     // let modalContent;
 
@@ -25,23 +17,42 @@ function PostsList({isPosting, onStopPosting}) {
     //             />
     //         </Modal>
     // }
+    const [posts, setPosts] = useState([]);
+
+    function addPostHandler(postData) {
+        setPosts((existingPosts) => [postData, ...existingPosts]);
+    }
+
     return (
         <>
             {isPosting && (
                 <Modal onClose={onStopPosting}>
                     <NewPost
-                        onBodyChange={bodyChangeHandler}
-                        onAuthorChange={authorChangeHandler}
+                        // onBodyChange={bodyChangeHandler}
+                        // onAuthorChange={authorChangeHandler}
                         onCancel={onStopPosting}
+                        onAddPost={addPostHandler}
                     />
                 </Modal>
             )}
-            <ul className={classes.posts}>
-                <Post author={enteredAuthor} body={enteredBody} />
-                <Post author="Hernawan" body="aaa" />
-            </ul>
+            {posts.length > 0 && (
+                <ul className={classes.posts}>
+                    {posts.map((post) => (
+                        <Post key={post.body} author={post.author} body={post.body}
+                        />
+                    ))}
+                </ul>
+            )}
+            {posts.length === 0 && (
+                <div style={{textAlign: 'center', color: 'white'}}>
+                    <h2>There are no posts yet.</h2>
+                    <p>Start adding some!</p>
+                </div>
+            )}
         </>
     );
 }
 
 export default PostsList;
+{/* <Post author={enteredAuthor} body={enteredBody} />
+                <Post author="Hernawan" body="aaa" /> */}
